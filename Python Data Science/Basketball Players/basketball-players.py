a5a40749c8e2d80ccd57d4a88940e9008c3a24e7
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-05-31 10:29:00 +0000

    Create basketball-players.py

---
+++ Python Data Science/Basketball Players/basketball-players.py
@@ -0,0 +1,11 @@
+players = [180, 172, 178, 185, 190, 195, 192, 200, 210, 190]
+
+mean = sum(players) / len(players)
+
+stdvar = (sum((v-mean)**2 for v in players)/len(players))**0.5
+
+low, high = mean-stdvar,mean+stdvar
+
+count = len([v for v in players if low < v < high])
+
+print(count)
