5ddafefbe58eca4ff4ac95548c44f3e073be4289
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-09 08:23:19 +0000

    Create chess-tournament.java

---
+++ Java/Introduction to Java/Chess Tournament Scores/chess-tournament.java
@@ -0,0 +1,10 @@
+public class Program {
+    public static void main(String[] args) {
+        int wins = 54;
+        int ties = 31;
+
+        System.out.println(wins + 0.5 * ties);
+
+        
+    }
+}
