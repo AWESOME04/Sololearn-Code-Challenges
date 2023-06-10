29f7b291f5dfcdcab49a50bc58fde84b5ec19662
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-10 12:55:17 +0000

    Create tip-calculator.java

---
+++ Java/Introduction to Java/Tip Calculator/tip-calculator.java
@@ -0,0 +1,10 @@
+import  java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        //your code goes here
+        Scanner sc = new Scanner(System.in);
+        double bill = sc.nextDouble();
+        System.out.println(bill * 15/100);
+    }
+}
