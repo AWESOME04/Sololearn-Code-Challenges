8d7bbc005e8f1160eb4d6b913b47017d6f8a79e9
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-06-19 13:41:05 +0000

    Create class_obj.java

---
+++ Java/Java Intermmediate/Creating Classes & Objects/class_obj.java
@@ -0,0 +1,18 @@
+public class Main {
+   public static void main(String[] args) {
+       
+       //create a Loading object with the same name
+        Loading loading = new Loading();
+       
+       
+       loading.LoadingMessage();
+   }
+}
+
+class Loading {
+     //complete the class, add LoadingMessage() method
+     public static void LoadingMessage(){
+         System.out.println("Loading");
+    }
+     
+}
