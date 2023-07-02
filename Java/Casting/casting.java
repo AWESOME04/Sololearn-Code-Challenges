0203fdb93cddff13e85d6689f3d5d18c1965f9c5
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-07-02 07:32:02 +0000

    Create casting.java

---
+++ Java/Casting/casting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       char a = read.next().charAt(0);
+       
+       //your code goes here
+
+       int b = (int) a;
+       System.out.println(b);
+       
+       
+    }   
+}
