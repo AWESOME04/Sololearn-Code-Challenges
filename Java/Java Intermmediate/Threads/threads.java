c0f4e37028d058c9b82ad28d228751c3e6e8bb61
Evans Acheampong <102630199+AWESOME04@users.noreply.github.com>
2023-07-10 14:01:01 +0000

    Create threads.java

---
+++ Java/Java Intermmediate/Threads/threads.java
@@ -0,0 +1,31 @@
+class Main {
+    public static void main(String[ ] args) {
+        Name name = new Name();
+        //set priority
+        name.setPriority(1);
+        
+        Welcome welcome = new Welcome();
+        //set priority
+        welcome.setPriority(6);
+        
+        welcome.start();
+        name.start();
+        
+    }
+}
+
+//extend the Thread class
+class Welcome extends Thread{
+    public void run() {
+        System.out.println("Welcome!");
+    }
+}
+
+//extend the Thread class
+class Name extends Thread{
+    public void run() {
+        System.out.println("Please enter your name");
+    }
+}
+
+
